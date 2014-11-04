d2c15cb700d63100edb9a090c199a8bdbd520ded
Fernando M. de Oliveira <fernando.dsfw@gmail.com>
2014-11-03 22:14:10 -0300

    Initial commit

---
+++ app/src/androidTest/java/socialgym/dce/ufpb/br/socialgym/ApplicationTest.java
@@ -0,0 +1,13 @@
+package socialgym.dce.ufpb.br.socialgym;
+
+import android.app.Application;
+import android.test.ApplicationTestCase;
+
+/**
+ * <a href="http://d.android.com/tools/testing/testing_android.html">Testing Fundamentals</a>
+ */
+public class ApplicationTest extends ApplicationTestCase<Application> {
+    public ApplicationTest() {
+        super(Application.class);
+    }
+}
