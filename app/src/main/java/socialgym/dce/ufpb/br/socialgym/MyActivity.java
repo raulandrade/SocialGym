b5c53594957e45033b63dbf26b1f4d297ccaea2f
Fernando M. de Oliveira <fernando.dsfw@gmail.com>
2014-11-07 09:58:18 -0300

    Configuração do Facebook SDK

--- app/src/main/java/socialgym/dce/ufpb/br/socialgym/MyActivity.java
+++ app/src/main/java/socialgym/dce/ufpb/br/socialgym/MyActivity.java
@@ -1,9 +1,13 @@
 package socialgym.dce.ufpb.br.socialgym;
 
 import android.app.Activity;
+import android.content.Context;
 import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.util.AttributeSet;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
 
 
 public class MyActivity extends Activity {
@@ -12,25 +16,5 @@ public class MyActivity extends Activity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_my);
-    }
-
-
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.my, menu);
-        return true;
-    }
-
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
-        if (id == R.id.action_settings) {
-            return true;
-        }
-        return super.onOptionsItemSelected(item);
     }
 }
